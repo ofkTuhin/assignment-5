1d3f066f1b2d5d377e53ca51404ccfd9c4f1bbc6
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-08 00:01:06 +0600

    final

--- api.js
+++ api.js
@@ -26,24 +26,64 @@ const geatMeal=mealItem=>{
    itemDetail.innerText=foodItem.strMeal
    foodDetail.appendChild(itemDetail)
 
-   
+   foodDetail.addEventListener('click',function(){
+    foodDetail.style.display='none'
+     const itemdes= document.createElement('div')
+     foods.appendChild(itemdes)
+     itemdes.className='itemdes'
+
+     const itempic=document.createElement('img')
+     itempic.src=foodItem.strMealThumb
+     itemdes.appendChild(itempic)
+
+     const itemid=document.createElement('h4')
+     itemid.innerText=foodItem.idMeal
+     itemdes.appendChild(itemid)
 
 
-  
+     const itemName=document.createElement('h5')
+     itemName.innerText='name: '+foodItem.strMeal
+     itemdes.appendChild(itemName)
+
+    
+
+
+   })
+
+
+   document.getElementById('submit').addEventListener('click',function(){
+    const food=document.getElementById('food').value
+   if(food==foodItem.strMeal)
+   {
+     foodDetail.style.display='block'
+   }
+   else{
+
+   }
+  })
 
 
  }
       
   };
 
-  document.getElementById('submit').addEventListener('click',function(){
-   const meal=document.getElementById('mealDiv')
-   meal.style.display='block'
-  })
+  // document.getElementById('submit').addEventListener('click',function(){
+  //   const food=document.getElementById('food').value
+  //  if(food=data.meals.strMeal)
+   
+  // })
 
 
   fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
 .then(res=>res.json())
 .then(data=>console.log(data))
+fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+.then(res=>res.json())
+.then(data=>console.log(data))
 
+
+
+
+
+  
 
