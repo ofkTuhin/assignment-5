09f2c2ea879a66096719d9feda8eea6309c6b89e
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-09 15:07:18 +0600

    late commit

--- api.js
+++ api.js
@@ -1,106 +1,94 @@
 
-fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
-.then(res=>res.json())
-.then(data=>geatMeal(data))
+document.getElementById('submit').addEventListener('click',function(){
+  const food=document.getElementById('food').value
+  if(food==''){
+    document.getElementById('meals').style.display='none'
+    document.getElementById('erormsg').style.display='block'
+  }
+  
 
-const geatMeal=mealItem=>{
-  mealItems=mealItem.meals
-  const foods=document.getElementById('foods')
-
-  for(let i=0;i<mealItems.length;i++){
-
-       const foodItem=mealItems[i] 
+ else{
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+food+'')
+  .then(res=>res.json())
+  .then(data=>getMeal(data))
+  const getMeal=mealItem=>{
+    const mealItems=mealItem.meals
+   
+    const foods=document.getElementById('foods')
+    
+    mealItems.forEach(foodItem  => {
       
     
+  
+     
 
-         const foodinfo=`
+        const foodinfo=`
+       
+       <img src="${foodItem .strMealThumb}">
+       <h5>${foodItem .strMeal}</h5>
         
-        <img src="${foodItem .strMealThumb}">
-        <h5>${foodItem .strMeal}</h5>
-         
-         `
+        `
   
-         
-  
-  
-         const foodDetail=document.createElement('div')
-     foods.appendChild(foodDetail)
-     foodDetail.innerHTML=foodinfo
-      
-          foods.className="foods"
-          foodDetail.className='foodDetail'
-
-          document.getElementById('submit').addEventListener('click',function(){
-            const food=document.getElementById('food').value
-           
-          if(food==foodItem.strMeal)
+        const foodDetail=document.createElement('div')
+    foods.appendChild(foodDetail)
+    foodDetail.innerHTML=foodinfo
+     
+         foods.className="foods"
+         foodDetail.className='foodDetail'
+        if (food=='') {
+          foodDetail.style.display='none'
           
-            {
-             
-             foodDetail.style.display='block'
-           }
-           else{
-
-            document.getElementById('erormsg').style.display='block'
-
-        
-           }
-           document.getElementById('food').value=''
-           foodDetail.addEventListener('click',function(){
-
-            fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
-.then(res=>res.json())
-.then(data=>
-  {
-
-    const x=data.meals
-    const y=x[0]
-    console.log(y.strIngredient1)
-     const foods=document.getElementById('foods')
+        }
+          foodDetail.style.display='block'
+          foodDetail.addEventListener('click',function(){
             foodDetail.style.display='none'
-             const itemdes= document.createElement('div')
-             foods.appendChild(itemdes)
-             itemdes.className='itemdes'
-             itemdes.style.display='block'
-
-             const itemInfo=`
-            <img src="${foodItem.strMealThumb}">
-            <h2>${y.strArea}</h2>
-            <h3>${y.idMeal}</h3>
-            <h5>${y.strIngredient1}</h5>
-            <h5>${y.strIngredient2}</h5>
-            <h5>${y.strIngredient3}</h5>
-            <h5>${y.strIngredient4}</h5>
-            <h5>${y.strIngredient5}</h5>
-            <h5>${y.strIngredient6}</h5>
-            <h5>${y.strIngredient7}</h5>
-            <h5>${y.strIngredient8}</h5>
-            <h5>${y.strIngredient9}</h5>
             
+  
+            const foods=document.getElementById('foods')
+                  
+                    const itemdes= document.createElement('div')
+                    foods.appendChild(itemdes)
+                    itemdes.className='itemdes'
+                    itemdes.style.display='block'
+         
+                    const itemInfo=`
+                   <img src="${foodItem.strMealThumb}">
+                   <h2>Street :${foodItem.strArea}</h2>
+                   <h3>id :${foodItem.idMeal}</h3>
+                   <h5>${foodItem.strIngredient1} : ${foodItem.strMeasure1}</h5>
+                   <h5>${foodItem.strIngredient2} : ${foodItem.strMeasure2}</h5>
+                   <h5>${foodItem.strIngredient3} : ${foodItem.strMeasure3}</h5>
+                   <h5>${foodItem.strIngredient4} : ${foodItem.strMeasure4}</h5>
+                   <h5>${foodItem.strIngredient5} : ${foodItem.strMeasure5}</h5>
+                   <h5>${foodItem.strIngredient6} : ${foodItem.strMeasure6}</h5>
+                   <h5>${foodItem.strIngredient7} : ${foodItem.strMeasure7}</h5>
+                   <h5>${foodItem.strIngredient8} : ${foodItem.strMeasure8}</h5>
+                   <h5>${foodItem.strIngredient9} : ${foodItem.strMeasure9}</h5>
+                  
+                    `
+                    itemdes.innerHTML=itemInfo
+         })
+           
+         
+         
 
-             
-             `
-             itemdes.innerHTML=itemInfo
-  })
-        
- })
+
+     
  
-   })
-      
-  };
+  document.getElementById('food').value=''
 
-}
+ 
+  
 
+  });
+  }
+ }
+})
+ 
  
 
   
    
   
 
-fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
-.then(res=>res.json())
-.then(data=>console.log(data))
 
-fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
-.then(res=>res.json())
-.then(data=>console.log(data))
