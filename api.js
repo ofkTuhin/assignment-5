27aee0071b86423ac3b79d209740414dc61afa4d
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-09 22:57:29 +0600

    final

--- api.js
+++ api.js
@@ -1,57 +1,58 @@
 
-document.getElementById('submit').addEventListener('click',function(){
-  const food=document.getElementById('food').value
-  if(food==''){
-    document.getElementById('meals').style.display='none'
-    document.getElementById('erormsg').style.display='block'
+document.getElementById('submit').addEventListener('click', function () {
+  const food = document.getElementById('food').value
+  if (food == '') {
+    document.getElementById('meals').style.display = 'none'
+    document.getElementById('erormsg').style.display = 'block'
   }
-  
 
- else{
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+food+'')
-  .then(res=>res.json())
-  .then(data=>getMeal(data))
-  const getMeal=mealItem=>{
-    const mealItems=mealItem.meals
-   
-    const foods=document.getElementById('foods')
-    
-    mealItems.forEach(foodItem  => {
-      
-    
-  
-     
 
-        const foodinfo=`
+  else {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + food + '')
+      .then(res => res.json())
+      .then(data => getMeal(data))
+      .catch(err =>document.getElementById('error').style.display='block')
+    const getMeal = mealItem => {
+      const mealItems = mealItem.meals
+
+      const foods = document.getElementById('foods')
+
+      mealItems.forEach(foodItem => {
+
+
+
+
+
+        const foodinfo = `
        
-       <img src="${foodItem .strMealThumb}">
-       <h5>${foodItem .strMeal}</h5>
+       <img src="${foodItem.strMealThumb}">
+       <h5>${foodItem.strMeal}</h5>
         
         `
-  
-        const foodDetail=document.createElement('div')
-    foods.appendChild(foodDetail)
-    foodDetail.innerHTML=foodinfo
-     
-         foods.className="foods"
-         foodDetail.className='foodDetail'
-        if (food=='') {
-          foodDetail.style.display='none'
-          
+
+        const foodDetail = document.createElement('div')
+        foods.appendChild(foodDetail)
+        foodDetail.innerHTML = foodinfo
+
+        foods.className = "foods"
+        foodDetail.className = 'foodDetail'
+        if (food == '') {
+          foodDetail.style.display = 'none'
+
         }
-          foodDetail.style.display='block'
-          foodDetail.addEventListener('click',function(){
-            foodDetail.style.display='none'
-            
-  
-            const foods=document.getElementById('foods')
-                  
-                    const itemdes= document.createElement('div')
-                    foods.appendChild(itemdes)
-                    itemdes.className='itemdes'
-                    itemdes.style.display='block'
-         
-                    const itemInfo=`
+        foodDetail.style.display = 'block'
+        foodDetail.addEventListener('click', function () {
+
+
+
+          const foods = document.getElementById('foods')
+
+          const itemdes = document.createElement('div')
+          foods.appendChild(itemdes)
+          itemdes.className = 'itemdes'
+          itemdes.style.display = 'block'
+
+          const itemInfo = `
                    <img src="${foodItem.strMealThumb}">
                    <h2>Street :${foodItem.strArea}</h2>
                    <h3>id :${foodItem.idMeal}</h3>
@@ -66,29 +67,29 @@ document.getElementById('submit').addEventListener('click',function(){
                    <h5>${foodItem.strIngredient9} : ${foodItem.strMeasure9}</h5>
                   
                     `
-                    itemdes.innerHTML=itemInfo
-         })
-           
-         
-         
+          itemdes.innerHTML = itemInfo
+        })
 
 
-     
- 
-  document.getElementById('food').value=''
 
- 
-  
 
-  });
+
+
+
+        document.getElementById('food').value = ''
+
+
+
+
+      });
+    }
   }
- }
 })
- 
- 
 
-  
-   
-  
+
+
+
+
+
 
 
